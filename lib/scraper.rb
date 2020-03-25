35064c9afa7ad1283bb56e18e5b5e1b8d17da89e
Terrence Bell <terrencekbell@gmail.com>
2020-03-25 23:56:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -35,3 +35,8 @@ class Scraper
 end
 student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
 student[:bio] = profile_page.css("div.description-holder p").text 
+
+student 
+end 
+end 
+
