60aa7a2a6453f0f912d2fd0132dbdb2624f5af4a
Terrence Bell <terrencekbell@gmail.com>
2020-03-25 23:07:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     page = Nokogiri::HTML(open(index_url))
     students = []
-    page.css("student")
+    page.css("student-card"). each do |student|
   end
 
   def self.scrape_profile_page(profile_url)
