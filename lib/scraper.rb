211aa230336c3de2045b25b931157e5942d6675b
Terrence Bell <terrencekbell@gmail.com>
2020-03-25 23:17:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,11 @@ class Scraper
     page.css("student-card"). each do |student|
       name = student.css("student-name").text 
       location = student.css("student-location").text 
-      profile_url = student.css(a)
+      profile_url = student.css("a").attr("href").value 
+      student info = {:name => name, :location => location, :profile_url => profile_url}
+    end 
+  end 
+  
   end
 
   def self.scrape_profile_page(profile_url)
