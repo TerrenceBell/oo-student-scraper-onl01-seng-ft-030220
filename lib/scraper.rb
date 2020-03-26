aadb3f99824de2a63a5f825ec1f188f0c4568d46
Terrence Bell <terrencekbell@gmail.com>
2020-03-26 00:03:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,7 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    students = {} 
+    student = {} 
     profile_page = Nokogiri::HTML(open(profile_url))
     links = profile_page.css(".social-icon-container a").map{|i| i.attribute("href").value}
     links.each do |link|
