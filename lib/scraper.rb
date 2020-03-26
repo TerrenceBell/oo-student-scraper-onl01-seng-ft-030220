82e3234d69022c86421358cffd68122dbfe2c793
Terrence Bell <terrencekbell@gmail.com>
2020-03-26 00:02:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     students = {} 
     profile_page = Nokogiri::HTML(open(profile_url))
-    links = profile_page.css(".social-icon-container").children.css("a").map{|i| i.attr("href").value}
+    links = profile_page.css(".social-icon-container a").map{|i| i.attribute("href").value}
     links.each do |link|
       if link.include?("linkedin")
         student[:linkedin] =link 
